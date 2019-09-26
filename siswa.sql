fe629f06a8ee29c132cb1c90ba797d33a354cd75
Dwi Candra Permana <dwichan@outlook.com>
2019-09-26 20:37:41 +0700

    Simple Database
    Changelog:
    - [NEW] Database berisi 1 tabel yang memiliki 1 row

---
+++ siswa.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Sep 2019 pada 15.36
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `daftar_siswa`
+--
+
+CREATE TABLE `daftar_siswa` (
+  `nis` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `kelas` varchar(15) NOT NULL,
+  `alamat` text NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` char(1) NOT NULL,
+  `telepon` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `daftar_siswa`
+--
+
+INSERT INTO `daftar_siswa` (`nis`, `nama`, `kelas`, `alamat`, `tanggal_lahir`, `jenis_kelamin`, `telepon`) VALUES
+('15160432', 'Dwi Candra Permana', 'XII RPL 3', 'Purwokerto Utara', '2000-05-09', 'l', '082121824775');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `daftar_siswa`
+--
+ALTER TABLE `daftar_siswa`
+  ADD PRIMARY KEY (`nis`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
